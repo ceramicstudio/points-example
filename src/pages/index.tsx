fdcf06f9b7d901186b89b13ee94ad4115d4782ff
Mark Krasner <mzkrasner@gmail.com>
2024-05-07 10:08:58 -0400

    Fixing button rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -348,7 +348,7 @@ const AuthShowcase: React.FC = () => {
           </div>
         </>
       )}
-      {address && (
+      {loggedIn && (
         <button
           className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
           onClick={() => {
